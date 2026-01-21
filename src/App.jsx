7c64981ddf4258f49663564933024e1a4f0d0c93
Nandhinichelluboina <nandhuchnandhini@gmail.com>
2026-01-21 12:06:32 +0530

    Remove welcome message from App component

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Welcome to the Project!</h2>
+    
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
